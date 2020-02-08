b4b7222c111031c9640a8fb608a12d50334fda5c
gaiz <stan_gai@Hotmail.com>
2020-02-08 23:37:22 +1100

    wip

---
+++ Go/questions/RemovePalindromicSubsequences.go
@@ -0,0 +1,14 @@
+package questions
+
+func RemovePalindromeSub(s string) int {
+	if len(s) == 0 {
+		return 0
+	}
+
+	for i := 0; i < len(s)/2; i++ {
+		if s[i] != s[len(s)-1-i] {
+			return 2
+		}
+	}
+	return 1
+}
---
+++ Go/tests/RemovePalindromicSubsequences_test.go
@@ -0,0 +1,13 @@
+// package tests
+
+// import (
+// 	"testing"
+
+// 	"github.com/leetcode/questions"
+// )
+
+// func TestRemovePalindromeSub(t *testing.T) {
+// 	input := "ababa"
+// 	expected := 1
+// 	actual := questions.RemovePalindromeSub(input)
+// }
