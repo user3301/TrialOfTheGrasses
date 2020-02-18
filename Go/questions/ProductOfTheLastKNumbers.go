5a1205065901265062939ae5cd2cfc269fe1b129
gaiz <stan_gai@hotmail.com>
2020-02-19 09:45:00 +1100

    product of the last k numbers without test cases

---
+++ Go/questions/ProductOfTheLastKNumbers.go
@@ -0,0 +1,28 @@
+package questions
+
+type ProductOfNumbers struct {
+	prefixProduct []int
+}
+
+func Constructor() ProductOfNumbers {
+	return ProductOfNumbers{
+		prefixProduct: []int{1},
+	}
+}
+
+func (p *ProductOfNumbers) Add(num int) {
+	if num > 0 {
+		p.prefixProduct = append(p.prefixProduct, p.prefixProduct[len(p.prefixProduct)-1] * num)
+	} else {
+		p.prefixProduct = []int{1}
+	}
+}
+
+func (p *ProductOfNumbers) GetProduct(k int) int {
+	 n := len(p.prefixProduct)
+	 if k >= n {
+	 	return 0
+	 } else {
+	 	return p.prefixProduct[n-1] / p.prefixProduct[n - k -1]
+	 }
+}
---
+++ Go/tests/ProductOfTheLastKNumbers_test.go
@@ -0,0 +1,12 @@
+package tests
+//
+//import "testing"
+//
+////"ProductOfNumbers","add","add","add","add","add","getProduct","getProduct","getProduct","add","getProduct"
+////[[],[3],[0],[2],[5],[4],[2],[3],[4],[8],[2]]
+//func TestCase1(t *testing.T) {
+//	input := map[string]int{
+//		"ProductOfNumbers": nil,
+//		"add":
+//	}
+//}
