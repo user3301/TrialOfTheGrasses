e213ed8baf280bd74f09092ad57ed9f34cb516ce
gaiz <stan_gai@hotmail.com>
2020-05-04 22:14:54 +1000

    optimized solution

--- Go/CheckIfAll1sAreAtLeastLengthKPlaceAway.go
+++ Go/CheckIfAll1sAreAtLeastLengthKPlaceAway.go
@@ -1,20 +1,16 @@
 package gosoln
 
 func kLengthApart(nums []int, k int) bool {
-	gap, i := 0, 0
-	for nums[i] == 0 && i < len(nums)-1 {
-		i++
-	}
+	count := k
 
-	for j := i; j < len(nums); j++ {
-		if nums[j] == 1 && j != i {
-			if gap >= k {
-				gap = 0
-			} else {
+	for _, num := range nums {
+		if num == 1 {
+			if count < k {
 				return false
 			}
+			count = 0
 		} else {
-			gap++
+			count++
 		}
 	}
 	return true
