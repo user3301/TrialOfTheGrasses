6a268d7c7b8b6a411175a1625cf50b4dce1eb0e0
gaiz <stan_gai@hotmail.com>
2020-05-08 10:19:40 +1000

    :white_check_mark: 1438

--- Go/LongestContinuousSubarrayWithAbsoluteDiffLessThanOrEqualToLimit_test.go
+++ Go/LongestContinuousSubarrayWithAbsoluteDiffLessThanOrEqualToLimit_test.go
@@ -1,14 +1,31 @@
 package gosoln
 
-import(
+import (
 	"testing"
 
 	"gotest.tools/assert"
 )
-func TestLongestSubarray(t *testing.T) {
-	arr := []int {8,2,4,7}
+
+func TestLongestSubarray1(t *testing.T) {
+	arr := []int{8, 2, 4, 7}
 	limit := 4
 	expected := 2
+	actual := LongestSubarray(arr, limit)
+	assert.Equal(t, expected, actual)
+}
+
+func TestLongestSubarray2(t *testing.T) {
+	arr := []int{10, 1, 2, 4, 7, 2}
+	limit := 5
+	expected := 4
+	actual := LongestSubarray(arr, limit)
+	assert.Equal(t, expected, actual)
+}
+
+func TestLongestSubarray3(t *testing.T) {
+	arr := []int{4, 2, 2, 2, 4, 4, 2, 2}
+	limit := 0
+	expected := 3
 	actual := LongestSubarray(arr, limit)
 	assert.Equal(t, expected, actual)
 }
