cddf9ffd4db97b882063a9e9d930bf55f8385380
gaiz <stan_gai@hotmail.com>
2020-05-31 18:41:28 +1000

    q3 AC solution

--- Go/Pseudo-PalindromicPathsInABinaryTree.go
+++ Go/Pseudo-PalindromicPathsInABinaryTree.go
@@ -3,68 +3,50 @@ package gosoln
 import . "github.com/leetcode/types"
 
 type TreeItem struct {
-	Vals []int
-	Node *TreeNode
+	Bitmap int
+	Node   *TreeNode
 }
 
 func PseudopalindromicPaths(root *TreeNode) int {
 	ans := 0
-	stack := make([]*TreeItem, 0)
-	var vals []int
-	vals = append(vals, root.Val)
-	item := &TreeItem{
-		Vals: vals,
-		Node: root,
+	bit := 0
+	bit ^= 1 << root.Val
+	rootItem := &TreeItem{
+		Bitmap: bit,
+		Node:   root,
 	}
-	stack = append(stack, item)
-
+	stack := make([]*TreeItem, 0)
+	stack = append(stack, rootItem)
 	for len(stack) != 0 {
 		size := len(stack)
 		for i := 0; i < size; i++ {
 			n := len(stack) - 1
 			current := stack[n]
 			stack = stack[:n]
-			// is leaf
 			if current.Node.Left == nil && current.Node.Right == nil {
-				if isPalindromeList(current.Vals) {
+				if current.Bitmap == 0 || (current.Bitmap&(current.Bitmap-1) == 0) {
 					ans++
 				}
 			}
 			if current.Node.Left != nil {
-				vals := append(current.Vals, current.Node.Left.Val)
+				bits := current.Bitmap
+				bits ^= 1 << current.Node.Left.Val
 				item := &TreeItem{
-					Vals: vals,
-					Node: current.Node.Left,
+					Bitmap: bits,
+					Node:   current.Node.Left,
 				}
 				stack = append(stack, item)
 			}
 			if current.Node.Right != nil {
-				vals := append(current.Vals, current.Node.Right.Val)
+				bits := current.Bitmap
+				bits ^= 1 << current.Node.Right.Val
 				item := &TreeItem{
-					Vals: vals,
-					Node: current.Node.Right,
+					Bitmap: bits,
+					Node:   current.Node.Right,
 				}
 				stack = append(stack, item)
 			}
 		}
 	}
 	return ans
-}
-
-func isPalindromeList(vals []int) bool {
-	dist := make(map[int]int)
-	seenOdd := false
-	for _, v := range vals {
-		dist[v]++
-	}
-	for _, d := range dist {
-		if d%2 != 0 {
-			if seenOdd {
-				return false
-			} else {
-				seenOdd = true
-			}
-		}
-	}
-	return true
 }
