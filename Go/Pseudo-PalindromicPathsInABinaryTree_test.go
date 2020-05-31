26345cb9aa1723afad00621960b635fa4eb51d0e
gaiz <stan_gai@hotmail.com>
2020-05-31 18:45:00 +1000

    comment out test cases

--- Go/Pseudo-PalindromicPathsInABinaryTree_test.go
+++ Go/Pseudo-PalindromicPathsInABinaryTree_test.go
@@ -33,30 +33,30 @@ func TestPseudopalindromicPaths1(t *testing.T) {
 	assert.Equal(t, expected, actual)
 }
 
-func TestIsPalindrome1(t *testing.T) {
-	s := []int{2, 3, 3}
-	expected := true
-	actual := isPalindromeList(s)
-	assert.Equal(t, expected, actual)
-}
-
-func TestIsPalindrome2(t *testing.T) {
-	s := []int{2, 1, 1}
-	expected := true
-	actual := isPalindromeList(s)
-	assert.Equal(t, expected, actual)
-}
-
-func TestIsPalindrome3(t *testing.T) {
-	s := []int{2, 3, 1}
-	expected := false
-	actual := isPalindromeList(s)
-	assert.Equal(t, expected, actual)
-}
-
-func TestIsPalindrome4(t *testing.T) {
-	s := []int{9, 5, 5, 5, 1, 1, 9, 5}
-	expected := true
-	actual := isPalindromeList(s)
-	assert.Equal(t, expected, actual)
-}
+//func TestIsPalindrome1(t *testing.T) {
+//	s := []int{2, 3, 3}
+//	expected := true
+//	actual := isPalindromeList(s)
+//	assert.Equal(t, expected, actual)
+//}
+//
+//func TestIsPalindrome2(t *testing.T) {
+//	s := []int{2, 1, 1}
+//	expected := true
+//	actual := isPalindromeList(s)
+//	assert.Equal(t, expected, actual)
+//}
+//
+//func TestIsPalindrome3(t *testing.T) {
+//	s := []int{2, 3, 1}
+//	expected := false
+//	actual := isPalindromeList(s)
+//	assert.Equal(t, expected, actual)
+//}
+//
+//func TestIsPalindrome4(t *testing.T) {
+//	s := []int{9, 5, 5, 5, 1, 1, 9, 5}
+//	expected := true
+//	actual := isPalindromeList(s)
+//	assert.Equal(t, expected, actual)
+//}
