004e76da2014c76c2638f11cf12ba9eefffdb4af
gaiz <stan_gai@hotmail.com>
2020-06-07 21:36:35 +1000

    optimise code

--- Go/MaximumAreaOfAPieceOfCakeAfterHorizontalAndVerticalCuts.go
+++ Go/MaximumAreaOfAPieceOfCakeAfterHorizontalAndVerticalCuts.go
@@ -7,17 +7,15 @@ import (
 
 func MaxArea1465(h int, w int, horizontalCuts []int, verticalCuts []int) int {
 	maxhi, maxvi := 0, 0
+	horizontalCuts = append(horizontalCuts, []int{0, h}...)
+	verticalCuts = append(verticalCuts, []int{0, w}...)
 	sort.Ints(horizontalCuts)
 	sort.Ints(verticalCuts)
-	horizontalCuts = append([]int{0}, horizontalCuts...)
-	horizontalCuts = append(horizontalCuts, h)
 	i := 0
 	for j := 1; j < len(horizontalCuts); j++ {
 		maxhi = max(maxhi, horizontalCuts[j]-horizontalCuts[i])
 		i++
 	}
-	verticalCuts = append([]int{0}, verticalCuts...)
-	verticalCuts = append(verticalCuts, w)
 	i = 0
 	for j := 1; j < len(verticalCuts); j++ {
 		maxvi = max(maxvi, verticalCuts[j]-verticalCuts[i])
