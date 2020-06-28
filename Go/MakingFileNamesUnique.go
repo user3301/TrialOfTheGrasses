b63a855df6f708f0d35ba16b765251c77d966311
gaiz <stan_gai@hotmail.com>
2020-06-28 11:54:24 +1000

    making file names unique

--- Go/MakingFileNamesUnique.go
+++ Go/MakingFileNamesUnique.go
@@ -2,37 +2,26 @@ package gosoln
 
 import (
 	"fmt"
-	"strconv"
 )
 
 func GetFolderNames(names []string) []string {
 	dist := make(map[string]int)
-	ans := make([]string, len(names))
-	for i, v := range names {
-		s := exists(dist, v)
-		ans[i] = s
+	ans := make([]string, 0)
+	for _, s := range names {
+		if _, exist := dist[s]; exist {
+			for i := dist[s] + 1; ; i++ {
+				newStr := fmt.Sprintf("%s(%d)", s, i)
+				if _, ex := dist[newStr]; !ex {
+					dist[newStr] = 0
+					dist[s] = i
+					ans = append(ans, newStr)
+					break
+				}
+			}
+		} else {
+			dist[s] = 0
+			ans = append(ans, s)
+		}
 	}
 	return ans
-}
-
-func exists(dist map[string]int, name string) string {
-	if _, ex := dist[name]; !ex {
-		dist[name]++
-		return name
-	} else if ex {
-		newStr := fmt.Sprintf("%s(1)", name)
-		return exists(dist, newStr)
-	} else if name[len(name)-1:] == ")" {
-		aa := name[len(name)-1:]
-		fmt.Println(aa)
-		i := name[len(name)-2 : len(name)-1]
-		ii, err := strconv.Atoi(i)
-		if err != nil {
-			panic(err)
-		}
-		newStr := fmt.Sprintf("%s(%d)", name[:len(name)-3], ii+1)
-		return exists(dist, newStr)
-	} else {
-		panic("a")
-	}
 }
