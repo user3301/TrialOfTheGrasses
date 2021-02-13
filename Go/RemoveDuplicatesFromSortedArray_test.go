668b720389ea877fa544e45704f69b9dfb5e9fd8
user3301 <stan_gai@hotmail.com>
2021-02-13 21:54:42 +1100

    more concise code for remove duplicates from sorted array (#46)

--- Go/RemoveDuplicatesFromSortedArray_test.go
+++ Go/RemoveDuplicatesFromSortedArray_test.go
@@ -1,18 +1,37 @@
 package gosoln
 
 import (
+	"fmt"
 	"testing"
 
 	"gotest.tools/assert"
 )
 
-func TestRemoveDuplicatesFromSortedArray1(t *testing.T) {
-	nums := []int{1, 1, 2}
-	expected := 2
-	expectedArr := []int{1, 2}
-	RemoveDuplicates(nums)
+func TestRemoveDuplicatesFromSortedArray(t *testing.T) {
+	testCases := []struct {
+		nums          []int
+		expected      int
+		expectedArray []int
+	}{
+		{
+			nums:          []int{1, 1, 2},
+			expected:      2,
+			expectedArray: []int{1, 2},
+		},
+		{
+			nums:          []int{0, 0, 1, 1, 1, 2, 2, 3, 3, 4},
+			expected:      5,
+			expectedArray: []int{0, 1, 2, 3, 4},
+		},
+	}
 
-	for i := 0; i < expected; i++ {
-		assert.Equal(t, expectedArr[i], nums[i])
+	for i, tc := range testCases {
+		tc := tc
+		t.Run(fmt.Sprintf("TestCase%d", i), func(t *testing.T) {
+			RemoveDuplicates(tc.nums)
+			for i := 0; i < tc.expected; i++ {
+				assert.Equal(t, tc.expectedArray[i], tc.nums[i])
+			}
+		})
 	}
 }
